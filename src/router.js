063121783aa7e6d996201b4f881536b221a42d40
Nurkhankazi <nurkhan7494@gmail.com>
2024-11-25 23:37:34 +0600

    login add

--- src/router.js
+++ src/router.js
@@ -2,6 +2,12 @@ import { createWebHistory, createRouter } from "vue-router";
 
 const routes =  [
   {
+    path:'/',
+    asias:'/login',
+    name:'login',
+    component:()=> import('./components/LoginForm')
+},
+  {
     path: "/home",
     alias: "/home",
     name: "home",
