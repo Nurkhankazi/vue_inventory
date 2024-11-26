080a0ff2af75410cfe93f8a5d5d0a17381091da2
Nurkhankazi <nurkhan7494@gmail.com>
2024-11-26 23:18:56 +0600

    Sidebar Add

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <div id="app">
-    <div>
-
- <Header></Header>
-  <!-- load all other page/component -->
+   <div className="layout-wrapper layout-content-navbar">
+      <div className="layout-container">
+          <Sidebar></Sidebar>
+          <div className="layout-page">
+            <Header></Header>
+            <div className="content-wrapper">
     <router-view />
-
-    
-    <Footer></Footer>  
-
-  </div>
-    
-  </div>
+    <Footer></Footer>
+            <div className="content-backdrop fade"></div>
+            </div>
+          </div>
+      </div>
+      <div className="layout-overlay layout-menu-toggle"></div>
+    </div>
 </template>
 
 <script>
 import Header from './components/Include/Header.vue'
 import Footer from './components/Include/Footer.vue'
+import Sidebar from './components/Include/Sidebar.vue'
 
 export default {
   name: 'App',
   components: {
-    Header,Footer
+    Header,Footer,Sidebar
   }
 }
 </script>
